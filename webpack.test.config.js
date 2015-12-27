8e4e8eeead994bebb19cab406b009b39e2337fe7
Rubén Sospedra <sospedra.r@gmail.com>
2015-12-27 18:34:56 +0100

    fix(test): Fix the retrocomp with node 4-
    Added the webpack babel preset es2015

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -2,8 +2,18 @@ module.exports = {
   entry: './index.js',
   output: {
     path: './test/',
-    filename: `logatim.tmp.js`,
+    filename: 'logatim.tmp.js',
     library: 'logatim',
     libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    }]
   }
 }
