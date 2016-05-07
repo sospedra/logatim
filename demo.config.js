69d27b02737c167c6b26d0614c3e42eb900ac8a9
sospedra <sospedra.r@gmail.com>
2016-05-07 16:00:29 +0200

    chore: change tryout to playground to meet the standard ignore files defaults

--- demo.config.js
+++ demo.config.js
@@ -2,18 +2,18 @@ module.exports = {
   entry: './index.js',
   output: {
     path: './demo/',
-    filename: 'logatim.tryout.js',
+    filename: 'bundle.js',
     library: 'logatim',
-    libraryTarget: 'var',
+    libraryTarget: 'var'
   },
   module: {
-    loaders:[{
+    loaders: [{
       test: /\.js?$/,
       exclude: /(node_modules|bower_components)/,
       loader: 'babel',
       query: {
-        presets: ['es2015'],
-      },
-    }],
-  },
+        presets: ['es2015']
+      }
+    }]
+  }
 }
