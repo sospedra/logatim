15042c677cffb705b911a79f9f1168faaae58adc
sospedra <sospedra.r@gmail.com>
2016-05-10 01:22:55 +0200

    fix: Add casting for messages containing objects

--- test/test-logs.js
+++ test/test-logs.js
@@ -12,13 +12,28 @@ const CustomError = function CustomError (message) {
 CustomError.prototype = Object.create(Error.prototype)
 
 const dummies = {
-  customError: `{"message":"Logatim!","name":"CustomError"}`,
-  simpleObject: `{"cool":1337,"loga":"tim"}`,
-  deepObject: `{"go":{"more":[1,2,{"deeper":"FTW","loga":["t","i","m"]}]}}`,
-  listObject: '[1,3,3,7]'
+  customError: {
+    'message': 'Logatim!',
+    'name': 'CustomError'
+  },
+  simpleObject: {
+    'cool': 1337,
+    'loga': 'tim'
+  },
+  listObject: [1, 3, 3, 7],
+  deepObject: { 'go':
+    {'more':
+      [ 1, 2, {
+        'deeper': 'FTW',
+        'loga': ['t', 'i', 'm']
+      }
+    ]}
+  }
 }
 
-const getDummy = (key) => `${dummies[key]}${constants.EOL}`
+const getDummy = (key) => {
+  return `${JSON.stringify(dummies[key])}${constants.EOL}`
+}
 
 test('can print custom errors', function (t) {
   t.plan(1)
@@ -31,25 +46,9 @@ test('can print custom errors', function (t) {
 test('can print objects', function (t) {
   t.plan(3)
 
-  const simple = {
-    cool: 1337,
-    loga: 'tim'
-  }
-
-  const deep = {
-    go: {
-      more: [1, 2, {
-        deeper: "FTW",
-        loga: ['t', 'i', 'm']
-      }]
-    }
-  }
-
-  const list = [1, 3, 3, 7]
-
-  t.equal(logatim.raw(simple), getDummy('simpleObject'), ' without leveling')
-  t.equal(logatim.raw(deep), getDummy('deepObject'), ' with a deep nesting')
-  t.equal(logatim.raw(list), getDummy('listObject'), ' array-like')
+  t.equal(logatim.raw(dummies.simpleObject), getDummy('simpleObject'), ' without leveling')
+  t.equal(logatim.raw(dummies.listObject), getDummy('listObject'), ' array-like')
+  t.equal(logatim.raw(dummies.deepObject), getDummy('deepObject'), ' with a deep nesting')
 })
 
 // use custom functions
